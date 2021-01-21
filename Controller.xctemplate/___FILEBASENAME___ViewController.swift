a386a8a38cf7f54b5c49e4dd27bf1b460992d5cf
Marcin Gmyz <marcin.gmyz@apzumi.pl>
2021-01-21 20:33:34 +0100

    ViewController file template - base view controller

--- Controller.xctemplate/___FILEBASENAME___ViewController.swift
+++ Controller.xctemplate/___FILEBASENAME___ViewController.swift
@@ -3,15 +3,12 @@ import Domain
 import RxSwift
 import RxCocoa
 
-final class ___FILEBASENAME___: UIViewController, ___VARIABLE_productName:identifier___View {
-
-    private let disposeBag = DisposeBag()
-    private let ui = ___VARIABLE_productName:identifier___UI()
+final class ___FILEBASENAME___: ViewController<___VARIABLE_productName:identifier___UI>, ___VARIABLE_productName:identifier___View {
+    
     private var presenter: ___VARIABLE_productName:identifier___Presenter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        attach(ui: ui)
         presenter = ___VARIABLE_productName:identifier___Presenter(view: self)
     }
 }
